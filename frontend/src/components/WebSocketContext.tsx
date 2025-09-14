539832f0751127333d9eda238222dac1278547cd
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-09-14 20:42:03 +0530

    added the socket logic, updated code to handle clientid and lastmessage

--- frontend/src/components/WebSocketContext.tsx
+++ frontend/src/components/WebSocketContext.tsx
@@ -3,26 +3,40 @@ import React, { createContext, useContext, useEffect, useState } from 'react'
 type WebSocketContextType = {
   socket: WebSocket | null,
   isConnected: boolean,
+  clientId: string | null,
+  lastMessage: any | null
 }
 
 const WebSocketContext = createContext<WebSocketContextType>({
   socket: null,
   isConnected: false,
+  clientId: null,
+  lastMessage: null
 })
 
 export const useWebSocket = () => {
   return useContext(WebSocketContext)
 }
 
-export const WebSocketProvider = ({children}: {children: React.ReactNode}) => {
+export const WebSocketProvider = ({ children }: { children: React.ReactNode }) => {
   const [socket, setSocket] = useState<WebSocket | null>(null)
   const [isConnected, setIsConnected] = useState(false)
-
+  const [clientId, setClientId] = useState<string | null>(null)
+  const [lastMessage, setLastMessage] = useState<any | null>(null)
+  
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
     ws.onopen = () => {
       console.log("Websocket connected.")
       setIsConnected(true)
+    }
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      setLastMessage(data)
+      if (data.type === "welcome" && data.id){
+        console.log("Client Id is: ", data.id)
+        setClientId(data.id)
+      }
     }
     ws.onclose = () => {
       console.log("Websocket disconnected.")
@@ -32,10 +46,10 @@ export const WebSocketProvider = ({children}: {children: React.ReactNode}) => {
     return () => {
       ws.close()
     }
-  },[])
+  }, [])
 
-  const value = {socket, isConnected}
-  
+  const value = { socket, isConnected, clientId, lastMessage }
+
   return (
     <WebSocketContext.Provider value={value}>
       {children}
