abb5e33838f3caecc34a3c30f9798377d094b2be
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-09-14 20:18:05 +0530

    remove usage of extra socket connection and make use of lastMessage

--- frontend/src/components/CreateRoom.tsx
+++ frontend/src/components/CreateRoom.tsx
@@ -1,11 +1,30 @@
-import { useRef} from "react"
+import { useEffect, useRef, useState } from "react"
 import Header from "./Header"
 import { useWebSocket } from "./WebSocketContext";
 
-const CreateRoom = ({ onRoomCreated }: { onRoomCreated: () => void }) => {
+type CreateRoomProps = {
+  onRoomCreated: (roomId: string, count?: number) => void;
+};
+
+const CreateRoom = ({ onRoomCreated }: CreateRoomProps) => {
   const nameRef = useRef<HTMLInputElement>(null);
   const roomIDRef = useRef<HTMLInputElement>(null)
-  const { socket, isConnected } = useWebSocket();
+  const [newRoomId, setNewRoomId] = useState<string>('')
+  const { socket, isConnected, lastMessage } = useWebSocket();
+
+  useEffect(() => {
+    if(!lastMessage) return;
+    if (lastMessage && lastMessage.type === 'roomCreated') {
+      const newRoomId = lastMessage.payload.roomId
+      setNewRoomId(newRoomId)
+      const initialCount = lastMessage.payload.userCount;
+      onRoomCreated(newRoomId, initialCount);
+    }
+    if(lastMessage.type === 'joined'){
+      const {roomId, userCount} = lastMessage.payload;
+      onRoomCreated(roomId, userCount);
+    }
+  }, [onRoomCreated, lastMessage])
 
   const handleClick = () => {
     if (isConnected && socket && nameRef.current?.value) {
@@ -15,20 +34,12 @@ const CreateRoom = ({ onRoomCreated }: { onRoomCreated: () => void }) => {
           name: nameRef.current.value
         }
       }))
-      onRoomCreated();
     } else {
       console.log("Socket not connected or name is empty.")
     }
   }
   const handleJoin = () => {
-    console.log("--- DEBUGGING handleJoin ---");
-    console.log("Socket object:", socket);
-    console.log("Name input value:", nameRef.current?.value);
-    console.log("Room ID input value:", roomIDRef.current?.value);
-    console.log("--------------------------");
-
     if (socket && nameRef.current?.value && roomIDRef.current?.value) {
-      localStorage.setItem("roomId", roomIDRef.current.value)
       socket.send(JSON.stringify({
         type: "join",
         payload: {
@@ -36,7 +47,6 @@ const CreateRoom = ({ onRoomCreated }: { onRoomCreated: () => void }) => {
           roomId: roomIDRef.current.value
         }
       }))
-      onRoomCreated();
     } else {
       console.log("Error while joining room.")
     }
@@ -54,10 +64,11 @@ const CreateRoom = ({ onRoomCreated }: { onRoomCreated: () => void }) => {
           <input ref={roomIDRef} type="text" placeholder='Enter Room Code' className='text-neutral-100 p-2 w-full border focus:border-neutral-100 outline-none border-neutral-800 rounded-md' />
           <button onClick={handleJoin} className='cursor-pointer bg-white hover:bg-neutral-200 w-[30%] text-black rounded-lg font-semibold'>Join Room</button>
         </div>
-        <div className="bg-neutral-900 h-30 rounded-lg flex flex-col justify-center items-center">
-          <div className="text-neutral-500">Share this room id with you friend</div>
-          <h1 className="text-2xl mt-2">Room ID: <span>BD734</span></h1>
-        </div>
+        {newRoomId &&
+          <div className="bg-neutral-900 h-30 rounded-lg flex flex-col justify-center items-center">
+            <div className="text-neutral-500">Share this room id with you friend</div>
+            <h1 className="text-2xl mt-2">{`Room ID: ${newRoomId}`} </h1>
+          </div>}
       </div>
       <div>
       </div>
