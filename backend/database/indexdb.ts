6bd893b0231ec5f96d7ed22269d2d0501ea22e28
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-09-14 20:26:01 +0530

    update mongodb connection logic

--- backend/database/indexdb.ts
+++ backend/database/indexdb.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDb = async () => {
   try {
-      const connectionDB = await mongoose.connect(`${process.env.MONGODB_URL}chatroom`)
+      const connectionDB = await mongoose.connect(`${process.env.MONGODB_URL}`)
   console.log(`\n MongoDB connected !! DB HOST: ${connectionDB.connection.host}`)
   } catch (error) {
     console.log("MongoDB connection error: ", error)
