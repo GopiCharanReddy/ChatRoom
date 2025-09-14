f81db1dd7a6ff7eaae2d29cecc0bef42ffcda19d
GopiCharanReddy <gopicharanvgcr@gmail.com>
2025-09-14 20:14:23 +0530

    remove extra socket usage and make use of lastmessage, update handleClick logic

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -6,23 +6,38 @@ type Message = {
   text: string,
   sender: "me" | "them"
 }
-
-const Chat = () => {
+type ChatProps = {
+  roomId: string | null,
+  userCount: number;
+}
+const Chat = ({ roomId, userCount: initialUserCount }: ChatProps) => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const { socket } = useWebSocket()
+  const { socket, clientId, lastMessage } = useWebSocket()
   const inputRef = useRef<HTMLInputElement>(null);
+  const [userCount, setUserCount] = useState(initialUserCount)
 
   useEffect(() => {
-    if (socket) {
-      socket.onmessage = (event) => {
-        const newMessage = event.data;
-        console.log("Received:", newMessage)
-        setMessages(prevMessages => [...prevMessages, newMessage])
+    if (!lastMessage) return;
+    if (lastMessage) {
+      const count = lastMessage.userCount || lastMessage.userCount
+      if (count !== undefined) {
+        setUserCount(count)
+      }
+      if (lastMessage.type === 'chatMessage') {
+        if (lastMessage.senderId !== clientId)
+          setMessages(prevMessages => [...prevMessages, { text: lastMessage.text, sender: "them" }])
+      } else if (lastMessage.type === 'joined' || lastMessage.type === 'userLeft') {
+        if(lastMessage.senderId !== clientId && lastMessage.senderId)
+        setMessages(prevMessage => [...prevMessage, { text: lastMessage.message, sender: "them" }])
       }
     }
-  })
+    if (lastMessage.payload?.userCount !== undefined) {
+      setUserCount(lastMessage.payload.userCount)
+    }
+  }, [lastMessage, clientId])
 
-  const handleClick = () => {
+  const handleClick = (e?: React.FormEvent) => {
+    if(e) e.preventDefault()
     const message = inputRef.current?.value;
     if (!message) return;
     setMessages(prevMessage => [...prevMessage, { text: message, sender: "me" }]);
@@ -31,7 +46,7 @@ const Chat = () => {
         type: 'chat',
         payload: {
           message,
-          roomId: localStorage.getItem("roomID")
+          roomId: roomId
         }
       }));
     }
@@ -43,18 +58,18 @@ const Chat = () => {
     <div className='flex flex-col gap-y-4 border border-neutral-800 p-4 rounded-lg'>
       <Header />
       <div className='bg-neutral-800 p-2 rounded-lg flex justify-between'>
-        <div>Room Code: 808080</div>
-        <div>Users 2/2</div>
+        <div>{`Room Code: ${roomId}`}</div>
+        <div>{`Users ${userCount}/2`}</div>
       </div>
       <div className='border border-neutral-800 rounded-lg w-full p-4 h-80 flex flex-col overflow-auto [&::-webkit-scrollbar]:w-1.5
-  [&::-webkit-scrollbar-track]:bg-transparent
-  [&::-webkit-scrollbar-thumb]:bg-slate-500/40
-  [&::-webkit-scrollbar-thumb]:rounded-full
-  [&::-webkit-scrollbar-thumb:hover]:bg-slate-500'>{messages.map((message, index: number) => <div className={`p-2 mt-2 rounded-lg inset-0 w-fit max-w-80 break-words ${message.sender === 'me' ? 'bg-white text-black self-end' : 'bg-neutral-800 text-white self-start'}`} key={index}>{message.text}</div>)}</div>
-      <div className='flex gap-x-2'>
+    [&::-webkit-scrollbar-track]:bg-transparent
+    [&::-webkit-scrollbar-thumb]:bg-slate-500/40
+    [&::-webkit-scrollbar-thumb]:rounded-full
+    [&::-webkit-scrollbar-thumb:hover]:bg-slate-500'>{messages.map((message, index: number) => <div className={`p-2 mt-2 rounded-lg inset-0 w-fit max-w-80 break-words ${message.sender === 'me' ? 'bg-white text-black self-end' : 'bg-neutral-800 text-white self-start'}`} key={index}>{message.text}</div>)}</div>
+      <form onSubmit={handleClick} className='flex gap-x-2'>
         <input ref={inputRef} id='message' type="text" placeholder='Type a message' className=' border-neutral-800 p-3 rounded-lg focus:border-white border-2 focus:border-2 w-[80%]' />
-        <button onClick={handleClick} className='bg-white text-black font-semibold p-3 rounded-lg px-6 tracking-wider'>Send</button>
-      </div>
+        <button type='submit' className='bg-white text-black font-semibold p-3 rounded-lg px-6 tracking-wider'>Send</button>
+      </form>
     </div>
   )
 }
